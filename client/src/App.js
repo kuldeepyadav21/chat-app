20e4cbf5018e78655792d098f3eac63c686b99da
Kuldeep Yadav <kuldeepyadav20484@gmail.com>
2026-04-22 03:23:37 +0530

    connect backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5001");
+const socket = io("https://chat-app-server-qklq.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
